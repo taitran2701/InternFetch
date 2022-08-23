05722333d5784997b35ae155a698cb7ec5cc20e1
taitran2701 <80756684+taitran2701@users.noreply.github.com>
2022-08-23 14:30:17 +0700

    Create AttachmentConfiguration.cs

---
+++ BackEnd/InternBA/EntityConfigs/AttachmentConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using InternBA.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace InternBA.EntityConfigs
+{
+    public class AttachmentConfiguration : IEntityTypeConfiguration<Attachment>
+    {
+        public void Configure(EntityTypeBuilder<Attachment> builder)
+        {
+            builder.HasKey(a =>a.ID);
+
+            builder.HasOne(a => a.Post);
+
+            builder.HasMany(a=>a.Categories);
+        }
+    }
+}
