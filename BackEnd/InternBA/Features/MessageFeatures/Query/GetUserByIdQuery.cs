ee1e5c5fb4e16b07e7ba2c96acf4c6a7d7579212
xuanloi1999 <55650190+xuanloi1999@users.noreply.github.com>
2022-08-23 11:34:20 +0700

    Query message

--- BackEnd/InternBA/Features/MessageFeatures/Query/GetUserByIdQuery.cs
+++ BackEnd/InternBA/Features/MessageFeatures/Query/GetUserByIdQuery.cs
@@ -1,6 +1,27 @@
-﻿namespace InternBA.Features.MessageFeatures.Query
+﻿using InternBA.Infrastructure.Data;
+using MediatR;
+
+namespace InternBA.Features.MessageFeatures.Query
 {
-    public class GetUserByIdQuery
+    public class GetUserByIdQuery : IRequest<Message>
     {
+        public Guid ID { get; set; }
+
+        public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, Message>
+        {
+            private readonly InternBADBContext _context;
+            public GetUserByIdQueryHandler(InternBADBContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Message> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+            {
+                var message = await _context.Messages.FindAsync(request.ID);
+                if (message == null || message.DeleteAt != null) return default;
+
+                return message;
+            }
+        }
     }
 }
