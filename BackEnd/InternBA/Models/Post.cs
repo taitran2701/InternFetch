0d86069d473fff374a3e9317afbfbe6c5b3ab439
taitran2701 <80756684+taitran2701@users.noreply.github.com>
2022-08-23 14:31:18 +0700

    Update Post.cs

--- BackEnd/InternBA/Models/Post.cs
+++ BackEnd/InternBA/Models/Post.cs
@@ -1,17 +1,26 @@
-﻿namespace InternBA.Models
+﻿using InternBA.Interface;
+
+namespace InternBA.Models
 {
-    public class Post
+    public class Post : IBaseEntity
     {
         public Guid ID { get; set; }
-        public int UserID { get; set; }
-        public int Attachment { get; set; }
-        public int Reaction { get; set; }
         public string Content { get; set; }
-        public DateTime CreatedDate {get;set;}
-        public DateTime UpdatedDate {get;set;}
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public bool? IsDeleted { get; set; }
 
-        public ICollection<Reaction> Reactions { get; set; }
-        public ICollection<Comment> Comments { get; set; }        
-        public ICollection<Attachment> Attachments { get; set; }
+
+        public Guid UserID { get; set; }
+
+        public Guid Reaction { get; set; }
+        public virtual ICollection<Reaction> Reactions { get; set; }
+
+        public Guid CommentID { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
+
+
+        public Guid AttachmentID { get; set; }
+        public virtual ICollection<Category> Categories { get; set; }
     }
 }
