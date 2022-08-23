b49bc2383c121ffd47bc5599e9d45d5b333b6975
taitran2701 <80756684+taitran2701@users.noreply.github.com>
2022-08-23 14:30:24 +0700

    Create PostConfiguration.cs

---
+++ BackEnd/InternBA/EntityConfigs/PostConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using InternBA.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace InternBA.EntityConfigs
+{
+    public class PostConfiguration : IEntityTypeConfiguration<Post>
+    {
+        public void Configure(EntityTypeBuilder<Post> builder)
+        {
+            builder.HasKey(p => p.ID);
+
+            builder.Property(p => p.Content)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            //builder.HasMany(p => p.Reactions);
+
+            builder.HasMany(p => p.Categories);
+
+            builder.HasMany(p=>p.Comments);
+        }
+    }
+}
