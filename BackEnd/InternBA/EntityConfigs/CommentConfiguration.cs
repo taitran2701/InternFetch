11a249c2d354c12c79d492f5772a8fc9d870387b
taitran2701 <80756684+taitran2701@users.noreply.github.com>
2022-08-23 14:30:22 +0700

    Create CommentConfiguration.cs

---
+++ BackEnd/InternBA/EntityConfigs/CommentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using InternBA.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+namespace InternBA.EntityConfigs
+{
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.HasKey(c =>c.ID);
+
+            builder.HasOne(c => c.Post);
+
+            builder.Property(c => c.Content)
+                .HasColumnName("Content")
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.HasMany(c => c.Reactions);
+       
+
+        }
+    }
+}
