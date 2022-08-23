4aed60a8c530b347f1fddf4ee13d0789191f80ea
taitran2701 <80756684+taitran2701@users.noreply.github.com>
2022-08-23 14:31:07 +0700

    Update InternBADBContext.cs

--- BackEnd/InternBA/InternBADBContext.cs
+++ BackEnd/InternBA/InternBADBContext.cs
@@ -1,6 +1,8 @@
 ﻿using InternBA.EntityConfigs;
 using InternBA.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
 
 namespace InternBA.Infrastructure.Data
 {
@@ -29,6 +31,35 @@ namespace InternBA.Infrastructure.Data
         public InternBADBContext(DbContextOptions<InternBADBContext> options)
         : base(options)
         {
+
+
+namespace InternBA
+{
+    public class InternBADBContext : DbContext
+    {
+        public InternBADBContext(DbContextOptions<InternBADBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Reaction> Reactions { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new PostConfiguration());
+
+            builder.ApplyConfiguration(new ReactionConfiguration());
+
+            builder.ApplyConfiguration(new CommentConfiguration());
+
+            builder.ApplyConfiguration(new AttachmentConfiguration());
+
+            builder.ApplyConfiguration(new CateConfiguration());
+
         }
 
     }
