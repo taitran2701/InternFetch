4f06b3676efbaa4a4bb98bd0046142d44b2bd594
xuanloi1999 <55650190+xuanloi1999@users.noreply.github.com>
2022-09-01 17:04:26 +0700

    add friends

--- BackEnd/InternBA/Features/UserFeatures/Queries/GetUserFriendQueryHandler.cs
+++ BackEnd/InternBA/Features/UserFeatures/Queries/GetUserFriendQueryHandler.cs
@@ -17,21 +17,47 @@ namespace InternBA.Features.UserFeatures.Queries
             var user = _context.Users.Where(user =>
             user.Username == request.username).FirstOrDefault();
 
-            var rooms = _context.UserRoom.Include(userRoom => userRoom.Room)
-                .ToList();
-
-            List<User> friends = new List<User>();
+            var rooms = _context.Rooms.Where(room => room.User2 == user.ID || room.User1 == user.ID).ToList();
+            List<Guid> friends = new List<Guid>();
             foreach (var item in rooms)
             {
-                if(user.ID == item.Room.User1 || user.ID == item.Room.User2)
+                if (user.ID == item.User1)
                 {
-                    Console.WriteLine(user.Username);
-                    friends.Add(user);
+                    friends.Add(item.User2);
+                }
+                else
+                {
+                    friends.Add(item.User1);
                 }
             }
+            //var rooms = _context.UserRoom.Include(userRoom => userRoom.Room)
+            //    .ToList();
 
-            IEnumerable<User> friendResult = friends.Distinct<User>();
-            return friendResult;
+            //List<Guid> friends = new List<Guid>();
+            //foreach (var item in rooms)
+            //{
+            //    if(user.ID == item.Room.User1)
+            //    {
+            //        Console.WriteLine(user.Username);
+            //        friends.Add(item.Room.User2);
+            //    }
+            //    else if (user.ID == item.Room.User2)
+            //    {
+            //        friends.Add(item.Room.User2);
+            //    }
+            //    else
+            //    {
+            //        continue;
+            //    }
+            //}
+
+            
+            List<User> friendlList = new List<User>();
+            foreach (var item in friends)
+            {
+                friendlList.Add(_context.Users.Where(user => user.ID == item).FirstOrDefault());
+            }
+            return friendlList;
         }
     }
 }
--- FrontEnd/internba/src/components/Sideright/index.tsx
+++ FrontEnd/internba/src/components/Sideright/index.tsx
@@ -6,7 +6,37 @@ import * as React from "react";
 
 export interface ISideRightProps {}
 
+interface IUser {
+  username: string;
+  password: string;
+  email: string;
+  avater: string;
+  comments: string | null;
+  posts: string | null;
+  room: string | null;
+  userRooms: string | null;
+  id: string;
+  deleteAt: string | null;
+  createdDate: string;
+  updatedDate: string;
+}
 export default function SideRight(props: ISideRightProps) {
+  const [friends, setFriends] = React.useState<IUser[]>([]);
+
+  React.useEffect(() => {
+    fetch(`https://localhost:7076/api/Users/search?search=${e.target.value}`, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((friends) => {
+        return setFriends(friends);
+      });
+  }, []);
   return (
     <div className={styles.sidebarRight}>
       <div>
